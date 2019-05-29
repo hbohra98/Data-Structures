2c3fe882aefe025cfb43beb6b5efa18de18df7cd
Himanshu Bohra <hbohra1998@gmail.com>
2019-05-29 11:08:38 +0530

    Add files via upload

---
+++ levelorder.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	char data;
+	Node *left;
+	Node *right;
+}; 
+/*
+inserting as in binary search tree
+*/
+Node* Insert(Node *root,char data) {
+	if(root == NULL) {
+		root = new Node();
+		root->data = data;
+		root->left = root->right = NULL;
+	}
+	else if(data <= root->data) root->left = Insert(root->left,data);
+	else root->right = Insert(root->right,data);
+	return root;
+}
+/* Level order traversal */ 
+
+void levelorder(Node *root){
+	if(root==NULL)return ;
+	
+	//queue which store the nodes that are to be visited
+	
+	queue<Node *> Q;
+	
+	Q.push(root);
+	
+	while(!Q.empty()){
+		
+		Node *current=Q.front();
+			/*
+		remove the element as its visited and left and right are 
+		pushed onto the queue 
+		*/
+		
+		Q.pop();
+		
+		cout << current->data << " ";
+			
+		/*
+		push left and right as the traversal is level order and 
+		we have to traverse next level rather than depth
+		*/
+	
+		if(current->left!=NULL)Q.push(current->left);
+		if(current->right!=NULL)Q.push(current->right);		
+	}
+}
+int main(){
+
+	/*
+	
+	            H
+			   / \
+			  E   A
+			 / \   \
+			L   T   H
+    
+	*/
+
+
+	Node* root = NULL;
+	//Node* root = NULL;
+	root = Insert(root,'H'); root = Insert(root,'E');
+	root = Insert(root,'A'); root = Insert(root,'L'); 
+	root = Insert(root,'T'); root = Insert(root,'H');
+ 
+	levelorder(root);
+	
+}
